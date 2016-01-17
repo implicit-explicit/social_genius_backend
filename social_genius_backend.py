f67ee77bc32f48a6106d805e550b7bfae36faa8e
mrmrcoleman <mark@implicit-explicit.com>
2016-01-17 14:40:36 +0100

    neo4j REST connection working plus basic handling for the /groups endpoint

--- social_genius_backend.py
+++ social_genius_backend.py
@@ -1,13 +1,21 @@
 #! /Library/Frameworks/Python.framework/Versions/3.3/bin/python3.3
 
-from flask import Flask
+from flask import Flask, request
+
 import json
+from neo4jrestclient.client import GraphDatabase
 
 app = Flask(__name__)
+gdb = GraphDatabase("http://localhost:7474/db/data/")
 
-@app.route("/groups")
-def get_groups():
-    return(json.dumps(['groups', {'name': ('docker_randstad', 1565)}]))
+@app.route('/groups/<group_name>', methods=['GET'])
+def get_groups(group_name=None):
+    q = """MATCH (group:Group) WHERE group.name = '%s' RETURN group""" % (group_name)
+    results = gdb.query(q=q)
+    if len(results) == 0:
+        return 'Group name not found!\n', 404
+    else:
+        return(str(results[0]) + '\n'), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
