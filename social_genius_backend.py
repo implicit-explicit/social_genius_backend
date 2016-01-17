4fe626cccddadb27c2dff82790e67ac7c712c6a1
mrmrcoleman <mark@implicit-explicit.com>
2016-01-17 13:04:56 +0100

    Test endpoint working

--- social_genius_backend.py
+++ social_genius_backend.py
@@ -1,3 +1,13 @@
-#! /usr/local/bin/python3
+#! /Library/Frameworks/Python.framework/Versions/3.3/bin/python3.3
 
+from flask import Flask
+import json
 
+app = Flask(__name__)
+
+@app.route("/groups")
+def get_groups():
+    return(json.dumps(['groups', {'name': ('docker_randstad', 1565)}]))
+
+if __name__ == '__main__':
+    app.run(debug=True)
